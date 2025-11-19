06a30691e88218acf4c85e0c433f1c95e4b16d55
Meelan Bandara <155341696+MeelanB@users.noreply.github.com>
2025-11-19 18:17:55 +0530

    fix: semantic release setup for version bump, package and changelog updates

---
+++ release.config.ts
@@ -0,0 +1,19 @@
+module.exports = {
+  branches: ['main'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    ['@semantic-release/changelog', {
+      changelogFile: 'CHANGELOG.md',
+    }],
+    ['@semantic-release/npm', {
+      npmPublish: false,
+      pkgRoot: '.',
+    }],
+    ['@semantic-release/git', {
+      assets: ['CHANGELOG.md', 'package.json'],
+      message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+    }],
+    '@semantic-release/github'
+  ],
+};
